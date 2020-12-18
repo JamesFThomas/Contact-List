03a9ce11c89c8187a31c27ac7ec5a656eb4bafdb
James F. Thomas <james.fthomas@yahoo.com>
2020-12-18 13:05:13 -0600

    (add) api users POST route saving new User information in cloud database sucessfully

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const express = require('express');
 const router = express.Router()
 // import model for the user schema
 const User = require('../models/User')
+// import bcrypt package
+const bcrypt = require('bcryptjs');
 //import Express validator package functions to check for desired information in data transmitted via users routes
 const { check, validationResult } = require('express-validator');
 
@@ -22,7 +24,8 @@ router.post('/', [
     .isLength({
       min: 6
     })
-], (req, res)=>{
+], 
+async (req, res)=>{
   const errors = validationResult(req);
   if(!errors.isEmpty()){
     return res
@@ -32,7 +35,43 @@ router.post('/', [
       });
   }
 
-  res.send('passed')
+    // deconstruct needed data from req.body
+    const {name, email, password} = req.body;
+
+    try {
+      // Use mongoose.findOne() to query DB for user email
+      let user = await User.findOne({ email });
+      // If email found
+      if(user){
+        // return error message "user already exists"
+        return res.status(400).json({ msg: "User already in database" });
+      }
+      // If email not found
+      else{
+        // Create new entry in db for user with User model
+        user = new User({
+          name,
+          email,
+          password
+        });
+      }
+
+      // Create salt to encrypt user information before persisting in db
+      const salt = await bcrypt.genSalt(10);
+
+      // Use salt to hash user password prop of above new User object
+      user.password = await bcrypt.hash(password, salt)
+
+      // Save new User model in DB
+      await user.save();
+
+      // Confirm new User model saved in db
+      res.send('New User Saved');
+
+    } catch (error) {
+      console.error(error.message)
+      res.status(500).send('Server Error')
+    }
 
 });
 
