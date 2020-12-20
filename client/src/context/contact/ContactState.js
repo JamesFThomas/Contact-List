b550b57be4bee93639fe3f604fd64c839a29e4a4
James F. Thomas <james.fthomas@yahoo.com>
2020-12-20 11:26:43 -0600

    (add) contact context file tree created to access and update contact state globally

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,71 @@
+// Import React package with useReducer hook
+import React, { useReducer } from 'react';
+// Import uuid package to create custom ids
+import uuid from 'uuid';
+// Import contact context instance
+import ContactContext from './contactContext';
+// Import contact context reduce function to update state values
+import ContactReducer from './contactReducer';
+// Import variables from types.js
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  CLEAR_FILTER
+} from '../types';
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id:1,
+        name: 'James Thomas',
+        email: 'jamest@email.com',
+        phone: '111-111-1111',
+        type: 'personal'
+      },
+      {
+        id:2,
+        name: 'Kayla Turner',
+        email: 'kaylat@email.com',
+        phone: '222-222-2222',
+        type: 'personal'
+      },
+      {
+        id:3,
+        name: 'Hunter Gunner',
+        email: 'huntert@email.com',
+        phone: '333-333-3333',
+        type: 'professional'
+      }
+    ]
+  };
+
+  // Initialize useReducer hook to access contacts state values
+  const [state, dispatch] = useReducer(ContactReducer, initialState);
+
+                                            // Contact State Actions
+
+    // ADD a contact,
+    // DELETE a contact,
+    // SET the current contact,
+    // UPDATE the contact,
+    // FILTER contacts
+    // CLEAR the filter
+
+    // Return contact context provider element
+    return (
+      <ContactContext.Provider
+        // Set contact state & actions for access in app components
+        value={{
+          contacts: state.contacts
+        }}
+        >
+          {props.children}
+        </ContactContext.Provider>
+    )
+};
+
+export default ContactState;
---
+++ client/src/context/contact/contactContext.js
@@ -0,0 +1,8 @@
+// Import React and deconstruct createContext hook from package
+import { createContext } from 'react';
+
+// Initialize new context for user contacts
+const contactContext = createContext();
+
+// Export new context for user contacts
+export default contactContext;
