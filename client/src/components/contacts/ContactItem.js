12c938efd9a08f3c4fb23f2ed58601a57f6331cf
James F. Thomas <james.fthomas@yahoo.com>
2020-12-20 13:04:09 -0600

    (add) Contacts & ContactItem components created

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,5 +1,7 @@
 // Import React package
 import React from 'react'
+// IMport
+import PropTypes from 'prop-types';
 
 const ContactItem = ({ contact }) => {
   // Deconstruct contact information from props
@@ -8,10 +10,33 @@ const ContactItem = ({ contact }) => {
     <div className='card bg-light'>
       <h3 className='text-primary text-left'>
         {/* Display contact name and a badge conditionally on "type" of contact  */}
-        {name}{' '} <span className={'badge ' + (type === 'professional' ? 'badge-success':'badge-primary')}>{type}</span>
+        {name}{' '}
+        <span style={{ float: 'right' }}
+          className={'badge ' +
+          (type === 'professional' ? 'badge-success':'badge-primary')
+          }
+        >
+          {type.charAt(0).toUpperCase() + type.slice(1)}
+        </span>
       </h3>
+      <ul className='list'>
+        {email && ( <li>
+          <i className='fas fa-envelope-open'></i> {email}
+        </li>)}
+        {phone && ( <li>
+          <i className='fas fa-phone'></i> {phone}
+        </li>)}
+      </ul>
+      <p>
+        <button className='btn btn-dark btn-sm'> Edit </button>
+        <button className='btn btn-danger btn-sm'> Delete </button>
+      </p>
     </div>
   )
 }
+
+ContactItem.propTypes = {
+  contact: PropTypes.object.isRequired
+};
 
 export default ContactItem;
