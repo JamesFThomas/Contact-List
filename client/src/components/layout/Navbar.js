b2dd24c1255375a36c1cddb1a9f6f1aef1043352
James F. Thomas <james.fthomas@yahoo.com>
2020-12-23 12:35:20 -0600

    (add) application navbar will indicate when user logged in with conditonal link render

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,27 +1,50 @@
-import React from 'react';
+// IMpoet React package and hooks
+import React, { Fragment, useContext } from 'react';
+// Import prop types package to require data types
 import PropTypes from "prop-types";
+// Import Link element from react dom router package
 import {Link} from 'react-router-dom'
+// Import auth context object
+import AuthContext from '../../context/auth/authContext';
+
 
 const Navbar = ({ title, icon }) => {
+  // Initialize context object in component
+  const authContext = useContext(AuthContext);
 
+  // Destructor actions/ variable from context state object
+  const { isAuthenticated, logoutUser, user} = authContext;
+
+  // Split visible links based on user authentication
+  const authLinks = (
+    <Fragment>
+      <li> Hello { user && user.name}</li>
+      <li>
+        <a href='#!'>
+          <i className='fas fa-sign-out-alt'></i> <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </Fragment>
+)
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+          <Link to='/register'> Register </Link>
+        </li>
+        <li>
+          <Link to='/login'> Login </Link>
+        </li>
+    </Fragment>
+  )
   return (
     <div className='navbar bg-primary'>
       <h1>
         <i className={icon}/> {title}
       </h1>
       <ul>
-        <li>
-          <Link to='/'> Home </Link>
-        </li>
-        <li>
-          <Link to='/about'> About </Link>
-        </li>
-        <li>
-          <Link to='/register'> Register </Link>
-        </li>
-        <li>
-          <Link to='/login'> Login </Link>
-        </li>
+        {/* Conditionally Render Links */}
+        { isAuthenticated ? authLinks : guestLinks }
       </ul>
 
     </div>
